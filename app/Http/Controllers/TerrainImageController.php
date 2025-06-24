480ee2876fde984ddf8e3a6175c46d1707c15034
lyvathanak <ngovxnak@gmail.com>
2025-06-24 14:04:47 +0700

    Created Project

---
+++ app/Http/Controllers/TerrainImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TerrainImage;
+use App\Http\Requests\StoreTerrainImageRequest;
+use App\Http\Requests\UpdateTerrainImageRequest;
+
+class TerrainImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTerrainImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TerrainImage $terrainImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TerrainImage $terrainImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTerrainImageRequest $request, TerrainImage $terrainImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TerrainImage $terrainImage)
+    {
+        //
+    }
+}
